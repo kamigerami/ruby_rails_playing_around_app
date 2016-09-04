ccd541909448195346a1861a7a68bee2345c45a0
Kamjar Gerami <kami.gerami@gmail.com>
2016-09-04 22:56:13 +0200

    Add new shared pages for tabs and update presenter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,6 +12,6 @@ module ApplicationHelper
     klass ||= "#{object.class}Presenter".constantize
     presenter = klass.new(object, self)
     yield presenter if block_given?
-    presenter
+    presenter = ""
   end
 end
